d8188bf84a34befaf3fa79c5e06e1a9e25a5c9b8
kzz5 <rbman5@gmail.com>
2013-09-26 23:00:43 -0500

    hi

--- .mybashsetup.sh
+++ .mybashsetup.sh
@@ -2,7 +2,7 @@
 
 mv ~/.bashrc ~/.bashrc.bak -f
 mv ~/.dir_colors ~/.dir_colors.bak -f 
-mv ~/bin ~/"bin$(date).bak"
+#mv ~/bin ~/"bin$(date).bak"
 
 ln -s hal/.bashrc ~/.bashrc
 ln -s hal/.dir_colors ~/.dir_colors
